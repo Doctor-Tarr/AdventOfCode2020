b09339dbfcfbabd214a759d619a87ce07f389599
Doctor Tarr <DrTarr@tarrandphether.com>
2020-12-02 15:20:24 -0700

    validated answers day 2

--- Day2/day_2.py
+++ Day2/day_2.py
@@ -17,27 +17,18 @@ def check_password_count(pw_list):
     valid_count = 0
 
     for item in pw_list:
-        # print(item)
         rule = item.split(':')[0]
         pw = item.split(':')[1]
-        # print(rule)
-        # print(pw)
 
         rule_char = rule.split()[1]
         rule_min = int(rule.split()[0].split('-')[0])
         rule_max = int(rule.split()[0].split('-')[1])
 
-        # print(rule_char)
-        # print("  min: " + str(rule_min))
-        # print("  max: " + str(rule_max))
-
         # count number of times the rule character is in the pw
         count = pw.count(rule_char)
-        # print("\n" + str(count) + '  ' + rule_char + ' in '+ pw)
 
         # see if it matches the rule
         if rule_min <= count <= rule_max:
-            # print('  ' + str(count) + " IS between " + str(rule_min) + " and " + str(rule_max))
             valid_count += 1 
 
 
@@ -45,6 +36,27 @@ def check_password_count(pw_list):
 
 def check_password_possition(pw_list):
     valid_count = 0
+
+
+    for item in pw_list:
+        rule = item.split(':')[0]
+        pw = item.split(':')[1]
+
+        rule_char = rule.split()[1]
+        pos_1 = int(rule.split()[0].split('-')[0])
+        pos_2 = int(rule.split()[0].split('-')[1])
+
+        # print('Checking if ' + rule_char + ' is EITHER in position ' + str(pos_1) + ' OR ' + str(pos_2))
+        # print('  in:  ' + pw)
+
+        in_1 = rule_char == pw[pos_1]
+        in_2 = rule_char == pw[pos_2]
+
+        if in_1 != in_2:
+            print('VALID')
+            print(pw)
+            valid_count += 1
+
 
     print('Valid based on position: ' + str(valid_count))
     return
