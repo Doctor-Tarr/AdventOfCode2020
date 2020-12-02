15c8efdfab3fab632a441a7e408f4d6cf8cdb5c5
Doctor Tarr <DrTarr@tarrandphether.com>
2020-12-02 13:01:45 -0700

    verified results

---
+++ Day1/day_1.py
@@ -0,0 +1,25 @@
+import sys
+import itertools
+
+file = '/home/lawrence/gitsource/AdventOfCode2020/Day1/expenses.list'
+
+def process_expenses(filename, count):
+    
+    with open(filename, 'r') as f:
+
+        expense_ints = [int(i) for i in f.readlines()]
+
+        combo_tuple = [combo for combo in itertools.combinations(expense_ints, count) if sum(combo) == 2020]
+        combo = (combo_tuple[0])
+        print(' The ' + str(count) + ' expenses that add up to 2020 are: ')
+        print(*combo)
+
+        result = 1
+        for i in combo:
+            result = result * i
+
+        print('The product of those numbers is: ' + str(result))
+
+if __name__ == '__main__':
+    process_expenses(file, 2)
+    process_expenses(file, 3)
